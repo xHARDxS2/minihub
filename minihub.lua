aa2fb9c58ccb0f87692e088811de1ee86063acbc
XHARDxS2 <lourenzorosocabreira@gmail.com>
2026-01-07 16:30:06 -0300

    minihub.lua

--- minihub.lua
+++ minihub.lua
@@ -5,17 +5,21 @@ local Player = Players.LocalPlayer
 
 print("MiniHub carregado com sucesso")
 
--- exemplo simples pra testar
 Player.CharacterAdded:Connect(function()
     print("Personagem carregado")
 end)
 
 -- GUI teste
-local ScreenGui = Instance.new("ScreenGui", Player.PlayerGui)
-local TextLabel = Instance.new("TextLabel", ScreenGui)
+local ScreenGui = Instance.new("ScreenGui")
+ScreenGui.Name = "MiniHubGui"
+ScreenGui.ResetOnSpawn = false
+ScreenGui.Parent = Player:WaitForChild("PlayerGui")
 
+local TextLabel = Instance.new("TextLabel")
+TextLabel.Parent = ScreenGui
 TextLabel.Size = UDim2.new(0, 300, 0, 50)
 TextLabel.Position = UDim2.new(0.5, -150, 0.1, 0)
 TextLabel.Text = "MiniHub Funcionando ✅"
 TextLabel.BackgroundColor3 = Color3.fromRGB(30,30,30)
 TextLabel.TextColor3 = Color3.fromRGB(255,255,255)
+TextLabel.TextScaled = true
